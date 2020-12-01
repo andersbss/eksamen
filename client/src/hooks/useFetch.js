84637298ec8158d156239b41289e680c843a0a6e
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 17:34:56 +0100

    Custom hooks

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { request } from '../services/httpService';
+
+const useFetch = (method, endpoint, payload = null) => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        setLoading(true);
+        const {
+          data: { success, data },
+        } = await request(method, endpoint, payload);
+        if (success) setResponse(data);
+        else setError(data);
+        setIsSuccess(success);
+      } catch (error) {
+        setError({ success: false, data: 'Unexpected error occurred' });
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, [method, endpoint, payload]);
+  return { error, loading, response, isSuccess };
+};
+
+export default useFetch;
