e8297d6414d812b02a8fc88b19a786cf08cef796
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-01 16:46:37 +0100

    AuthController register user

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,2 +1,7 @@
 import { userService } from '../services/index.js';
-import response from '../utils/response.js';
+import { sendToken } from '../utils/jwt.js';
+
+export const register = async (req, res, next) => {
+  const user = await userService.createUser(req.body);
+  sendToken(user, res);
+};
