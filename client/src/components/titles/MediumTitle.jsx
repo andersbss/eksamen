3aab72884de14f40d0df9d6981bdec29aec6d6a8
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 16:01:47 +0100

    Added title components

---
+++ client/src/components/titles/MediumTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h2`
+  color: black;
+  text-align: center;
+`;
+
+const MediumTitle = ({ content }) => <StyledTitle>{content}</StyledTitle>;
+
+export default MediumTitle;
