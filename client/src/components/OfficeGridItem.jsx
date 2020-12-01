a78d460da0586cfe52d2f9550ebe72cbea35def9
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 20:55:18 +0100

    Changed size and font of GridItem

--- client/src/components/OfficeGridItem.jsx
+++ client/src/components/OfficeGridItem.jsx
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 const StyledGridItem = styled.div`
   border: 5px solid black;
   height: 200px;
-  width: 200px;
+  width: 250px;
   margin: 0px 25px 25px 25px;
 `;
 
 const OfficeGridItem = ({ office, address, phone, email }) => (
   <StyledGridItem>
-    <p>{office}</p>
+    <h3>{office}</h3>
     <p>{address}</p>
     <p>{phone}</p>
     <p>{email}</p>
