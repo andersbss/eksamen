7f1d6aeddeffb5c6d863833341d5ec502879c97b
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 20:44:37 +0100

    Created OfficeGridContainer - contains OfficeGridItems

---
+++ client/src/components/OfficeGridContainer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import MediumTitle from './titles/MediumTitle';
+import OfficeGridItem from './OfficeGridItem';
+
+const StyledGridContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const OfficeGridContainer = ({ offices }) => (
+  <>
+    <MediumTitle
+      content={`${offices.location} (${offices.offices.length} kontorer)`}
+    />
+    <StyledGridContainer>
+      {offices.offices.length <= 0 ? (
+        <p>404</p>
+      ) : (
+        offices.offices.map((office) => (
+          <OfficeGridItem
+            office={office.office}
+            address={office.address}
+            phone={office.phone}
+            email={office.email}
+          />
+        ))
+      )}
+    </StyledGridContainer>
+  </>
+);
+
+export default OfficeGridContainer;
