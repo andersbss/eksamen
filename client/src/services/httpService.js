0e78cb3b2af0438d86b7f88a270bc231d07f10c8
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 17:35:48 +0100

    Created http service

---
+++ client/src/services/httpService.js
@@ -0,0 +1,11 @@
+import http from '../utils/http';
+
+export const request = async (method, endpoint, payload = null) => {
+  try {
+    return await http({ method, url: endpoint, data: payload });
+  } catch (error) {
+    if (error.response) return error.response;
+    console.log(error);
+    return { data: { success: false, data: 'Connection error' } };
+  }
+};
