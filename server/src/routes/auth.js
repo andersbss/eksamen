3d998bdec2a8ab9279dbadd07c602d167dbefc0c
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-01 20:49:48 +0100

    Login working

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,5 +4,6 @@ import { authController } from '../controllers/index.js';
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 export default router;
