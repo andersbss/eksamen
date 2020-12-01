95ba18f2159f0fe9e68a3e920b84d2aa112f6bb7
Sigmund <sigmund.baklia@gmail.com>
2020-12-01 20:36:24 +0100

    hardkoda liste over kontorer

---
+++ client/src/offices.js
@@ -0,0 +1,91 @@
+const officeList = [
+  {
+    location: 'Fredrikstad',
+    offices: [
+      {
+        office: 'Rorlegger1',
+        address: 'rorleggerveien 1',
+        phone: '69990000',
+        email: 'fredrikstad1@epost.no',
+      },
+      {
+        office: 'Rorlegger2',
+        address: 'rorleggerveien 2',
+        phone: '69990000',
+        email: 'fredrikstad2@epost.no',
+      },
+      {
+        office: 'Rorlegger3',
+        address: 'rorleggerveien 3',
+        phone: '69990000',
+        email: 'fredrikstad3@epost.no',
+      },
+    ],
+  },
+  {
+    location: 'Sarpsborg',
+    offices: [
+      {
+        office: 'Rorlegger4',
+        address: 'rorleggerveien 4',
+        phone: '69990000',
+        email: 'sarpsborgd4@epost.no',
+      },
+      {
+        office: 'Rorlegger5',
+        address: 'rorleggerveien 5',
+        phone: '69990000',
+        email: 'sarpsborgd5@epost.no',
+      },
+      {
+        office: 'Rorlegger6',
+        address: 'rorleggerveien 6',
+        phone: '69990000',
+        email: 'sarpsborgd6@epost.no',
+      },
+      {
+        office: 'Rorlegger7',
+        address: 'rorleggerveien 7',
+        phone: '69990000',
+        email: 'sarpsborgd7@epost.no',
+      },
+      {
+        office: 'Rorlegger8',
+        address: 'rorleggerveien 8',
+        phone: '69990000',
+        email: 'sarpsborgd8@epost.no',
+      },
+      {
+        office: 'Rorlegger8',
+        address: 'rorleggerveien 8',
+        phone: '69990000',
+        email: 'sarpsborgd8@epost.no',
+      },
+    ],
+  },
+  {
+    location: 'Moss',
+    offices: [
+      {
+        office: 'Rorlegger9',
+        address: 'rorleggerveien 9',
+        phone: '69990000',
+        email: 'moss9@epost.no',
+      },
+      {
+        office: 'Rorlegger10',
+        address: 'rorleggerveien 10',
+        phone: '69990000',
+        email: 'moss10@epost.no',
+      },
+      {
+        office: 'Rorlegger11',
+        address: 'rorleggerveien 11',
+        phone: '69990000',
+        email: 'moss11@epost.no',
+      },
+    ],
+  },
+];
+
+export { officeList };
