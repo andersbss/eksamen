cbf26729e1e95a27f7d8261aea8cab14e3a9224b
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-01 16:59:44 +0100

    Get user by email service

--- server/src/services/user.js
+++ server/src/services/user.js
@@ -1,3 +1,8 @@
 import User from '../models/user.js';
 
 export const createUser = async (user) => User.create(user);
+
+export const getUserByEmail = async (email, includePassword) => {
+  if (includePassword) return User.findOne({ email }).select('+password');
+  return User.findOne({ email });
+};
