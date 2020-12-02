d733b5a8f215a10b6ac589fbb3d7535e83c12da2
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-02 19:08:43 +0100

    Replaced article mongoose schema with converted joigoose schema

--- server/src/models/article.js
+++ server/src/models/article.js
@@ -1,30 +1,13 @@
 import mongoose, { Schema } from 'mongoose';
-import { ARTICLE_CONTENT, ARTICLE_INGRESS, ARTICLE_TITLE } from '../constants/dataRules';
 
-const ArticleSchema = new Schema(
-  {
-    title: ARTICLE_TITLE,
-    ingress: ARTICLE_INGRESS,
-    content: ARTICLE_CONTENT,
-    author: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Author',
-      required: true,
-    },
-    category: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Category',
-      required: true,
-    },
-    publisher: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-  },
-  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
-);
+import { articleSchema } from '../schemas/article';
 
-const Author = mongoose.model('Article', ArticleSchema);
+const Joigoose = require('joigoose')(mongoose);
 
-export default Author;
+const options = { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } };
+
+const ArticleSchema = new Schema(Joigoose.convert(articleSchema, options));
+
+const Article = mongoose.model('Article', ArticleSchema);
+
+export default Article;
