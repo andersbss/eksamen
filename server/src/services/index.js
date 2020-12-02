482949389aec1d43db45782ce3c06985d13b1772
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-02 12:17:32 +0100

    Hooked up services to index.js

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,2 +1,4 @@
 export * as userService from './user.js';
 export * as categoryService from './category.js';
+export * as authorService from './author.js';
+export * as articleService from './article.js';
