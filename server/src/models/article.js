83893589d8e8f5af9f3e5b3b0c0885989f285698
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-02 11:14:15 +0100

    Updated article model

--- server/src/models/article.js
+++ server/src/models/article.js
@@ -5,6 +5,19 @@ const ArticleSchema = new Schema({
   title: ARTICLE_TITLE,
   ingress: ARTICLE_INGRESS,
   content: ARTICLE_CONTENT,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Author',
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  publisher: mongoose.Schema.ObjectId,
+  ref: 'User',
+  required: true,
 });
 
 const Author = mongoose.model('Article', ArticleSchema);
