41b3a1459649abf77b0c08202783dc8481c863c8
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-02 21:29:22 +0100

    Added regexes and fixed typos

--- server/src/schemas/article.js
+++ server/src/schemas/article.js
@@ -1,19 +1,18 @@
 import Joi from 'joi';
+import { OBJECT_ID_REGEX } from '../constants/regexes.js';
 import logErrors from '../utils/schemaErrorLogger.js';
-
-const OBJECT_ID_REGEX = /^[0-9a-fA-F]{24}$/;
 
 export const articleSchema = Joi.object().keys({
   title: Joi.string().required().max(50).messages({
     'any.required': 'Title is required',
     'string.empty': 'Title is required',
-    'string.max': 'Title cannot be longer than 50 digits',
+    'string.max': 'Title cannot be longer than 50 characters',
   }),
   ingress: Joi.string().max(1000).messages({
-    'string.max': 'Ingress cannot be longer than 1000 digits',
+    'string.max': 'Ingress cannot be longer than 1000 characters',
   }),
   content: Joi.string().max(3000).messages({
-    'string.max': 'Content cannot be longer than 3000 digits',
+    'string.max': 'Content cannot be longer than 3000 characters',
   }),
   author: Joi.string()
     .required()
