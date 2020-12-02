a7d18413fb0fd6f587cff3d37510ef5d0e6a53c0
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-02 11:40:53 +0100

    Completed models

--- server/src/models/article.js
+++ server/src/models/article.js
@@ -1,24 +1,27 @@
 import mongoose, { Schema } from 'mongoose';
 import { ARTICLE_CONTENT, ARTICLE_INGRESS, ARTICLE_TITLE } from '../constants/dataRules';
 
-const ArticleSchema = new Schema({
-  title: ARTICLE_TITLE,
-  ingress: ARTICLE_INGRESS,
-  content: ARTICLE_CONTENT,
-  author: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Author',
+const ArticleSchema = new Schema(
+  {
+    title: ARTICLE_TITLE,
+    ingress: ARTICLE_INGRESS,
+    content: ARTICLE_CONTENT,
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Author',
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    publisher: mongoose.Schema.ObjectId,
+    ref: 'User',
     required: true,
   },
-  category: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Category',
-    required: true,
-  },
-  publisher: mongoose.Schema.ObjectId,
-  ref: 'User',
-  required: true,
-});
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 const Author = mongoose.model('Article', ArticleSchema);
 
