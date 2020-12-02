f3fb6586010f4d884f8f3b190d4efd288f1799ec
Sigmund <sigmund.baklia@gmail.com>
2020-12-02 15:35:58 +0100

    Created Employees component for showing EmployeeItems

---
+++ client/src/components/Employees.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import EmployeeItem from './EmployeeItem';
+import { employeesList } from '../mockUpData';
+
+const StyledEmployeeContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 20px;
+`;
+
+const Employees = ({ officeId }) => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const getEmployees = (officeId) => {
+      employeesList.forEach((element) => {
+        if (element.officeId === parseInt(officeId)) {
+          setEmployees(element.employees);
+        }
+      });
+    };
+    if (officeId) {
+      getEmployees(officeId);
+    }
+  }, [officeId]);
+
+  return (
+    <StyledEmployeeContainer>
+      {employees.length <= 0 ? (
+        <p>Ingen ansatte</p>
+      ) : (
+        employees.map((employee) => (
+          <EmployeeItem navn={employee.navn} stilling={employee.stilling} />
+        ))
+      )}
+    </StyledEmployeeContainer>
+  );
+};
+export default Employees;
