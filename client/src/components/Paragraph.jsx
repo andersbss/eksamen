4f5ba29035eaca1b905e4c83b0d6c8f12d57da4d
Sigmund <sigmund.baklia@gmail.com>
2020-12-02 15:38:36 +0100

    Created generic component for Paragraphs with header

---
+++ client/src/components/Paragraph.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.section`
+  margin: 20px;
+  text-align: left;
+  width: 70%;
+`;
+
+const StyledTitle = styled.h2`
+  color: black;
+`;
+
+const Paragraph = ({ header, content }) => (
+  <StyledContainer>
+    <StyledTitle>{header}</StyledTitle>
+    <p>{content}</p>
+  </StyledContainer>
+);
+
+export default Paragraph;
