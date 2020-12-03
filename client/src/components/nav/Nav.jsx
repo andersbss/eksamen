038b1e81004b6479a275070ede617291883df59f
Sigmund <sigmund.baklia@gmail.com>
2020-12-03 06:57:58 +0100

    Moved navigation components out of Nav. And adds Desktop and Tablet nav components

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import MediumTitle from '../titles/MediumTitle';
+import DesktopNav from './DesktopNav';
+import TabletNav from './TabletNav';
 
 const StyledNav = styled.nav`
   margin: 0;
-  width: 100%;
   font-weight: bolder;
   height: 60px;
   padding: 15px;
-`;
-
-const StyledUl = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  display: flex;
-  float: right;
-`;
-
-const StyledLi = styled.li`
-  font-size: 2em;
-  margin-left: 20px;
-  color: black;
-
-  & > a {
-    text-decoration: none;
-
-    &:visited {
-      color: black;
-    }
-    &.active {
-      color: ${(props) => props.theme.colors.blue};
-      border-bottom: 4px solid ${(props) => props.theme.colors.blue};
-    }
-  }
 `;
 
 const StyledName = styled.div`
@@ -45,28 +18,8 @@ const StyledName = styled.div`
 const Nav = () => (
   <StyledNav>
     <StyledName>FG</StyledName>
-    <StyledUl>
-      <StyledLi>
-        <NavLink exact to="/hjem" activeClassName="active">
-          Hjem
-        </NavLink>
-      </StyledLi>
-      <StyledLi>
-        <NavLink exact to="/kontorer" activeClassName="active">
-          Kontorer
-        </NavLink>
-      </StyledLi>
-      <StyledLi>
-        <NavLink exact to="/fagartikler" activeClassName="active">
-          Fagartikler
-        </NavLink>
-      </StyledLi>
-      <StyledLi>
-        <NavLink exact to="/kontakt" activeClassName="active">
-          Kontakt
-        </NavLink>
-      </StyledLi>
-    </StyledUl>
+    <DesktopNav />
+    <TabletNav />
   </StyledNav>
 );
 export default Nav;
