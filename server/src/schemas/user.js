d861c9a1d7f722aa67954d22024707427c9755a0
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-03 09:58:33 +0100

    Removed comments

--- server/src/schemas/user.js
+++ server/src/schemas/user.js
@@ -38,10 +38,3 @@ export const userSchema = Joi.object()
       }),
   })
   .options({ abortEarly: false });
-
-// export const OBJECT_ID_REGEX = /^[0-9a-fA-F]{24}$/;
-// export const ONE_DIGIT_REGEX = /(?=.*[0-9])/;
-// export const ONE_LOWERCASE_REGEX = /(?=.*[a-z])/;
-// export const ONE_UPPERCASE_REGEX = /(?=.*[A-Z])/;
-// export const ONE_SPECIAL_CHARACTER = /(?=.*[!@#$%^&*])/;
-// export const EIGHT_CHARACTERS_OR_MORE_REGEX = /(?=.{8,})/;
