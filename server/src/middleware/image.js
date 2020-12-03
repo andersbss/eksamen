8f9240e760162ff4cab911c0278546e57fe22be2
Sigmund <sigmund.baklia@gmail.com>
2020-12-03 09:32:04 +0100

    Created Image middleware to handle image data stream with multer

---
+++ server/src/middleware/image.js
@@ -0,0 +1,28 @@
+import multer from 'multer';
+import ErrorHandler from '../utils/errorHandler';
+
+function fileFilter(req, file, cb) {
+  const filetypes = /\.(jpeg|jpg|png)$/;
+  if (!file.originalname.match(filetypes)) {
+    return cb(new ErrorHandler('Only .jpeg, .jpg and .png files can be uploaded', 400));
+  }
+  cb(null, true);
+}
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './public/images');
+  },
+  // Use date as custom value
+  filename(req, file, cb) {
+    const date = new Date();
+    const customValue = `${date.getUTCDate()}-${date.getUTCMonth()}-${date.getUTCFullYear()}-${date.getUTCHours()}_${date.getUTCMinutes()}_${date.getUTCSeconds()}_`;
+    cb(null, `${customValue}_${file.originalname}`);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 6000000 }, // 6MB file size limit
+  fileFilter,
+}).single('image');
