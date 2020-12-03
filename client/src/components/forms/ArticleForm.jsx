78880780c686a509da8c80986e339071296cb135
Sigmund <sigmund.baklia@gmail.com>
2020-12-03 16:46:33 +0100

    Created ArticleForm to create articles (NOT FINISHED)

--- client/src/components/forms/ArticleForm.jsx
+++ client/src/components/forms/ArticleForm.jsx
@@ -1,26 +1,57 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Button from '../common/Button';
+import Select from '../common/Select';
+import Error from '../errors/Error';
+import useFetch from '../../hooks/useFetch';
 
 const StyledFormContainer = styled.main`
-  width: 70%;
-`;
-
-const StyledForm = styled.form`
+  padding: 20px;
+  width: 80%;
   margin: auto;
 `;
 
+const StyledForm = styled.form`
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  & > * {
+  }
+`;
+
+const StyledLabel = styled.label`
+  font-size: 1.8rem;
+  font-weight: bold;
+  & > * {
+    display: block;
+    margin-bottom: 5px;
+    border: solid 2px ${(props) => props.theme.colors.grey};
+    border-radius: 5px;
+    height: 50px;
+    width: 100%;
+  }
+
+  & > textarea {
+    resize: none;
+  }
+`;
+
 const initialFormData = Object.freeze({
-  tittel: '',
+  title: '',
   ingress: '',
-  innhold: '',
-  dato: '',
-  kategori: '',
-  forfatter: '',
+  content: '',
+  published: '',
+  category: '',
+  author: '',
 });
 
 // tittel, ingress, innhold(textarea), dato, forfatter(nedtrekksliste), kategori(nedtrekksliste)
 const ArticleForm = () => {
   const [formData, updateFormData] = useState(initialFormData);
+
+  const { error, loading, response, isSuccess } = useFetch(
+    'GET',
+    '/categories'
+  );
 
   const handleChange = (e) => {
     updateFormData({
@@ -37,64 +68,73 @@ const ArticleForm = () => {
   return (
     <StyledFormContainer>
       <StyledForm>
-        <label>
+        <StyledLabel>
           Tittel
           <input
             type="text"
-            name="tittel"
+            name="title"
             placeholder="Tittel"
+            maxLength="50"
+            required
             onChange={handleChange}
           />
-        </label>
-        <br />
-        <label>
+        </StyledLabel>
+        <StyledLabel>
           Ingress
           <input
             type="text"
             name="ingress"
             placeholder="Ingress"
+            maxLength="1000"
+            required
             onChange={handleChange}
           />
-        </label>
-        <br />
-        <label>
+        </StyledLabel>
+        <StyledLabel>
           Innhold
           <textarea
-            name="innhold"
+            name="content"
             placeholder="Innhold"
+            maxLength="3000"
+            required
             onChange={handleChange}
           />
-        </label>
-        <br />
-        <label>
-          Dato
+        </StyledLabel>
+        <StyledLabel>
+          Publiseringsdato
           <input
             type="date"
-            name="dato"
-            placeholder="Dato"
+            name="published"
+            required
             onChange={handleChange}
           />
-        </label>
-        <br />
-        <label>
+        </StyledLabel>
+        <StyledLabel>
           Kategori
-          <select name="kategori" onChange={handleChange}>
-            <option value="Kategori1">Kategori1</option>
-            <option value="Kategori2">Kategori2</option>
-            <option value="Kategori3">Kategori3</option>
-          </select>
-        </label>
-        <br />
-        <label>
+          {loading && <p>Loading...</p>}
+          {isSuccess && (
+            <Select name="category" onChange={handleChange}>
+              {response.length <= 0 ? (
+                <p>Ingen kategorier</p>
+              ) : (
+                response.map((category) => (
+                  <option value={category.title}>{category.title}</option>
+                ))
+              )}
+            </Select>
+          )}
+          {!isSuccess && !loading && <Error error={error} />}
+        </StyledLabel>
+        <StyledLabel>
           Forfatter
-          <select name="forfatter" onChange={handleChange}>
+          <select name="author" required onChange={handleChange}>
+            <option value={null}>Velg en forfatter</option>
             <option value="Lars Larsen">Lars Larsen</option>
             <option value="Gunn Gundersen">Gunn Gundersen</option>
             <option value="Simen Simensen">Simen Simensen</option>
           </select>
-        </label>
-        <br />
-        <button onClick={handleSubmit}>Submit</button>
+        </StyledLabel>
+        <Button click={handleSubmit} content="Create" type="submit" />
       </StyledForm>
     </StyledFormContainer>
   );
