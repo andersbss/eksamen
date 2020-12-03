703ac4720a09618b2ab2b7c2709f16261a9361f3
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-03 15:44:31 +0100

    Data is featching and added loader animation

--- client/src/pages/articles/index.jsx
+++ client/src/pages/articles/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Loader from '../../components/animations/Loader';
 import Jumbotron from '../../components/common/Jumbotron';
 import ArticlesContainer from '../../components/containers/ArticlesContainer';
+import Error from '../../components/errors/Error';
+import ArticleList from '../../components/lists/ArticleList';
 import ArticlesToggles from '../../components/options/ArticlesToggles';
 import useFetch from '../../hooks/useFetch';
 
@@ -12,6 +15,12 @@ const Articles = () => {
       <Jumbotron headerText="Fagartikler" />
       <ArticlesContainer>
         <ArticlesToggles />
+        {loading && !isSuccess ? (
+          <Loader />
+        ) : (
+          <ArticleList articles={response} />
+        )}
+        {!isSuccess && !loading && <Error error={error} />}
       </ArticlesContainer>
     </>
   );
