5386f4a4aa341b0adb0e4a8bd46d8bd1b9ae7210
Sigmund <sigmund.baklia@gmail.com>
2020-12-04 09:32:52 +0100

    Added getAll to author controller

--- server/src/controllers/author.js
+++ server/src/controllers/author.js
@@ -6,3 +6,8 @@ export const create = catchAsyncErrors(async (req, res, next) => {
   const category = await authorService.createAuthor(req.body);
   response(res, 201, true, category);
 });
+
+export const getAll = catchAsyncErrors(async (req, res, next) => {
+  const authors = await authorService.getAllAuthors();
+  response(res, 201, true, authors);
+});
