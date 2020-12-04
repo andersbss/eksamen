c0371813461afb9a1051ce27e845cb8ed814c6ce
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-04 22:16:25 +0100

    Fixed options bug in schema

--- server/src/models/category.js
+++ server/src/models/category.js
@@ -5,7 +5,7 @@ const Joigoose = require('joigoose')(mongoose);
 
 const options = { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } };
 
-const CategorySchema = new Schema(Joigoose.convert(categorySchema, options));
+const CategorySchema = new Schema(Joigoose.convert(categorySchema), options);
 
 const Category = mongoose.model('Category', CategorySchema);
 
