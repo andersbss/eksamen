8db414d1271230a9bb1f8e37f2df319ca10f89ae
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-04 22:04:16 +0100

    Some detailArticle styling

--- client/src/components/items/ArticleDetailItem.jsx
+++ client/src/components/items/ArticleDetailItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledInfoContainer = styled.span`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const ArticleDetailItem = ({ article }) => {
   console.log(article);
 
   return (
     <article>
+      <StyledInfoContainer>
+        <h3>{`Av ${article.author.firstName} ${article.author.lastName}`}</h3>
+      </StyledInfoContainer>
+
       <p>{article.title}</p>
     </article>
   );
