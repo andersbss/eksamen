e35ca9bf81d63de691f301bc6fcd2c6d9990b3ca
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-05 18:33:41 +0100

    Refactored styling at Homagrid

--- client/src/components/grids/HomeGrid.jsx
+++ client/src/components/grids/HomeGrid.jsx
@@ -2,33 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import MediumTitle from '../titles/MediumTitle';
 
-const StyledSectionOffices = styled.section`
+const StyledDefaultHomeSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > h2 {
+    margin: 0;
+    font-size: 4rem;
+    font-weight: 900;
+  }
+`;
+
+const StyledSectionOffices = styled(StyledDefaultHomeSection)`
   grid-column-start: 1;
   grid-column-end: span 1;
   grid-row-start: 1;
   grid-row-end: 1;
   background-color: ${(props) => props.theme.colors.grey};
   height: 300px;
-  padding: 35px 0px 35px 0px;
 `;
-const StyledSectionContact = styled.section`
+
+const StyledSectionContact = styled(StyledDefaultHomeSection)`
   grid-column-start: 2;
   grid-column-end: span 2;
   grid-row-start: 1;
   grid-row-end: 1;
   background-color: ${(props) => props.theme.colors.grey};
   height: 300px;
-  padding: 35px 0px 35px 0px;
 `;
 
-const StyledSectionArticles = styled.section`
+const StyledSectionArticles = styled(StyledDefaultHomeSection)`
   grid-column-start: 1;
   grid-column-end: span 3;
   grid-row-start: 2;
   grid-row-end: 2;
   background-color: ${(props) => props.theme.colors.grey};
   height: 350px;
-  padding: 35px 0px 35px 0px;
 `;
 
 const HomeGrid = () => (
