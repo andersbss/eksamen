9a8c366573e05df2cf1586ceb1b6ca9315511105
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-05 08:37:28 +0100

    Styled article

--- client/src/components/articles/ArticleDetailArticle.jsx
+++ client/src/components/articles/ArticleDetailArticle.jsx
@@ -5,20 +5,36 @@ import formatDate from '../../utils/dateFormatter';
 const StyledInfoContainer = styled.span`
   display: flex;
   justify-content: space-between;
+
+  & > h3 {
+    font-size: 1.3rem;
+    font-weight: 900;
+  }
+`;
+
+const StyledArticle = styled.article`
+  & > p {
+    font-size: 2rem;
+    font-weight: 400;
+
+    &:nth-child(1) {
+      margin-bottom: 100px;
+    }
+  }
 `;
 
 const ArticleDetailArticle = ({ article }) => {
   console.log(article);
 
   return (
-    <article>
+    <StyledArticle>
       <StyledInfoContainer>
         <h3>{`Av ${article.author.firstName} ${article.author.lastName}`}</h3>
         <h3>{formatDate(article.createdAt)}</h3>
       </StyledInfoContainer>
-
-      <p>{article.title}</p>
-    </article>
+      <p>{article.ingress}</p>
+      <p>{article.content}</p>
+    </StyledArticle>
   );
 };
 
--- client/src/layouts/ArticleDetailLayout.jsx
+++ client/src/layouts/ArticleDetailLayout.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
+  margin: auto;
+  margin-top: 50px;
   display: grid;
+  width: 1000px;
 `;
 
 const ArticleDetailLayout = ({ children }) => (
