f796330c8d53e1b69ad71bc7e99b55e03d0d7d9b
Sigmund <sigmund.baklia@gmail.com>
2020-12-05 12:52:04 +0100

    Added label and style to select component

--- client/src/components/common/Select.jsx
+++ client/src/components/common/Select.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Select = ({ name, onChange, children }) => (
-  <select name={name} required onChange={onChange}>
-    {children}
-  </select>
+const StyledContainer = styled.span`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  row-gap: 3px;
+  & > label {
+    color: black;
+    font-size: 1.8rem;
+    font-weight: 600;
+  }
+  & > select {
+    height: 50px;
+    font-size: 2rem;
+    border-radius: 5px;
+    border: solid grey 2px;
+    color: ${(props) => props.theme.colors.black};
+    padding: 5px;
+    ::placeholder {
+      font-size: 1em;
+    }
+  }
+`;
+// MÅ POSISJONERE TIL HØYRE OSV (for de som ikke hater css)
+const StyledErrorLabel = styled.label``;
+
+const Select = ({ name, onChange, label, errorLabel, children }) => (
+  <StyledContainer>
+    <label>{label}</label>
+    {errorLabel && <StyledErrorLabel>{errorLabel}</StyledErrorLabel>}
+    <select name={name} required onChange={onChange}>
+      {children}
+    </select>
+  </StyledContainer>
 );
 
 export default Select;
