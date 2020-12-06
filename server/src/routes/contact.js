fcb540c4d7c310d529109fbc71177c5b4d621491
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 17:23:56 +0100

    Removed unused imports

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { ROLE } from '../constants/roles.js';
 import { contactController } from '../controllers/index.js';
-import { authenticate, authorize, authorizeAccess } from '../middleware/auth.js';
+import { authenticate, authorize } from '../middleware/auth.js';
 import validate from '../middleware/validate.js';
 import { contactSchema } from '../schemas/contact.js';
-import { contactService } from '../services/index.js';
 
 const router = express.Router();
 
