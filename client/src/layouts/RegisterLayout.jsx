e5c27329836e2ad44ae276622ba2c8774afa8ee9
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-06 17:01:54 +0100

    Added registerpage and registerlayout

---
+++ client/src/layouts/RegisterLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMain = styled.main``;
+
+const RegisterLayout = ({ children }) => {
+  <StyledMain>{children}</StyledMain>;
+};
+
+export default RegisterLayout;
