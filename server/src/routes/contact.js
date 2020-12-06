b20a132321837162c48ecee8a775350ac795da67
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 16:03:11 +0100

    Created Contact routes

---
+++ server/src/routes/contact.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { ROLE } from '../constants/roles.js';
+import { contactController } from '../controllers/index.js';
+import { authenticate, authorize } from '../middleware/auth.js';
+import validate from '../middleware/validate.js';
+import { contactSchema } from '../schemas/contact.js';
+
+const router = express.Router();
+
+router.get('/', contactController.getAll);
+router.post('/', contactController.create);
+
+export default router;
