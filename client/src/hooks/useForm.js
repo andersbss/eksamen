ba3ce4fc4ec8f294adfa3ab8c660150fad703457
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-06 19:18:00 +0100

    Fixed logic useForm

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 const useForm = (callBack, validate) => {
   const [errors, setErrors] = useState([]);
   const [inputs, setInputs] = useState({});
+  const [readySubmit, setReadySubmit] = useState(false);
 
   useEffect(() => {
     const execute = async () => {
       await callBack();
     };
-    if (errors.length === 0) execute();
-  }, [errors, callBack]);
+    if (Object.keys(errors).length === 0 && readySubmit) execute();
+  }, [errors, callBack, readySubmit]);
 
   useEffect(() => {
     setErrors(validate(inputs));
@@ -17,12 +18,14 @@ const useForm = (callBack, validate) => {
 
   const handleChange = (e) => {
     e.persist();
+    setReadySubmit(false);
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors(validate(inputs));
+    setReadySubmit(true);
   };
 
   return [inputs, errors, handleChange, handleSubmit];
