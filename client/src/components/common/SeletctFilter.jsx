dddc7cad441f8bcd96df6f82545e7b3850cc314a
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-06 09:10:33 +0100

    Created AddFilter component

---
+++ client/src/components/common/SeletctFilter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSelect = styled.select``;
+
+const SelectFilter = () => {
+  console.log();
+  // useFetch
+
+  return (
+    <StyledSelect>
+      <option>Kategori</option>
+      <option>Kategori</option>
+    </StyledSelect>
+  );
+};
+
+export default SelectFilter;
