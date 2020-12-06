0756362684d050dc065266285ce18c0ed5488794
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 16:10:19 +0100

    Sends mail when POST => /contacts (with name and message)

--- server/src/controllers/contact.js
+++ server/src/controllers/contact.js
@@ -1,8 +1,21 @@
 import catchAsyncErrors from '../middleware/catchAsync.js';
 import response from '../utils/response.js';
 import { contactService } from '../services/index.js';
+import { sendMail } from '../utils/sendEmail.js';
 
 export const create = catchAsyncErrors(async (req, res, next) => {
+  const { name, message } = req.body;
+
+  try {
+    await sendMail({
+      email: 'placeholder@email.no',
+      subject: `Denne meldingen ble sendt av ${name} til Rørleggerfirma FG via deres kontaktside.`,
+      message: `${message}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   const contact = await contactService.createContact(req.body);
   response(res, 201, true, contact);
 });
