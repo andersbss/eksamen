0a279b1fb479c1a3f50623040b1b3b9cd89fbc8d
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 16:55:37 +0100

    Removed name and added email

--- server/src/schemas/contact.js
+++ server/src/schemas/contact.js
@@ -2,15 +2,6 @@ import Joi from 'joi';
 
 export const contactSchema = Joi.object()
   .keys({
-    name: Joi.string()
-      .required()
-      .max(200)
-      .meta({ _mongoose: { trim: true } })
-      .messages({
-        'any.required': 'Name is required',
-        'string.empty': 'Name is required',
-        'string.max': 'Name cannot be longer than 200 characters',
-      }),
     message: Joi.string()
       .required()
       .max(400)
@@ -20,5 +11,9 @@ export const contactSchema = Joi.object()
         'string.empty': 'message is required',
         'string.max': 'message cannot be longer than 400 characters',
       }),
+    email: Joi.string().required().email().messages({
+      'any.required': 'Email is required',
+      'string.email': 'Invalid email',
+    }),
   })
   .options({ abortEarly: false });
