035c4ac9cdab10f9701193b8cb58261c32095324
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-06 11:06:50 +0100

    Additional styling to filter dropdown

--- client/src/components/common/SelectFilter.jsx
+++ client/src/components/common/SelectFilter.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const StyledSelect = styled.select`
+  font-size: ${(props) => props.theme.fonts.sizes.button};
+  font-weight: ${(props) => props.theme.fonts.weights.button};
   height: 85px;
   width: 200px;
-
   border: 0;
   background-color: ${(props) => props.theme.colors.lightGrey};
 
-  option {
-    text-align: center;
-    font-size: 1.5rem;
+  text-align-last: center;
 
+  option {
+    font-size: 1.5rem;
+    margin: auto;
     &:nth-child(1) {
       display: ${(props) => (props.optionSelected ? 'none' : 'block')};
     }
@@ -25,10 +27,7 @@ const SelectFilter = ({ categories }) => {
   useEffect(() => {
     if (selected) {
       setHeaderOption('INGEN');
-
-      if (selected === 'INGEN') {
-        setHeaderOption('FILTER');
-      }
+      if (selected === 'INGEN') setHeaderOption('FILTER');
     }
   }, [selected]);
 
