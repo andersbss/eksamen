329c473d13369c326c01d50096a5edf1311bba3f
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 17:23:06 +0100

    Added authentication so only admins can GET contacts

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { ROLE } from '../constants/roles.js';
 import { contactController } from '../controllers/index.js';
-import { authenticate, authorize } from '../middleware/auth.js';
+import { authenticate, authorize, authorizeAccess } from '../middleware/auth.js';
 import validate from '../middleware/validate.js';
 import { contactSchema } from '../schemas/contact.js';
+import { contactService } from '../services/index.js';
 
 const router = express.Router();
 
-router.get('/', contactController.getAll);
-router.post('/', contactController.create);
+router.get('/', authenticate, authorize(ROLE.ADMIN), contactController.getAll);
+router.get('/:id', authenticate, authorize(ROLE.ADMIN), contactController.getById);
+router.post('/', [validate(contactSchema)], contactController.create);
 
 export default router;
