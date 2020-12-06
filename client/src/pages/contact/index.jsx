b8f2fdb2a7f8abd18a99ae0d629ac2e309c3aa9f
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 19:49:26 +0100

    If user is logged in, send email as prop to ContactForm

--- client/src/pages/contact/index.jsx
+++ client/src/pages/contact/index.jsx
@@ -4,22 +4,23 @@ import Jumbotron from '../../components/common/Jumbotron';
 import ContactForm from '../../components/forms/ContactForm';
 import ContactLayout from '../../layouts/ContactLayout';
 import { request } from '../../services/httpService';
+import { useUserContext } from '../../context/UserContext';
 
 const Contact = () => {
   const [submitSuccess, setSubmitSuccess] = useState(false);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const { loggedIn, user } = useUserContext();
   const history = useHistory();
 
   const handleSubmit = async (e, email, message) => {
     e.preventDefault();
-
     try {
       setLoading(true);
       const {
         data: { success, data },
       } = await request('POST', '/contacts', { email, message });
+      console.log(user);
 
       if (success) {
         setSubmitSuccess(success);
@@ -46,6 +47,7 @@ const Contact = () => {
           loading={loading}
           error={error}
           submitSuccess={submitSuccess}
+          userEmail={loggedIn ? user.email : ''}
         />
       </ContactLayout>
     </>
