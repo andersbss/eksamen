9fcca72f4c11817369ff4bf5627e3aee0f1a5512
Sigmund <sigmund.baklia@gmail.com>
2020-12-06 17:21:35 +0100

    Added getContactById to contact service

--- server/src/services/contact.js
+++ server/src/services/contact.js
@@ -1,5 +1,7 @@
 import Contact from '../models/contact.js';
 
+export const getContactById = (id) => Contact.findById(id);
+
 export const getAllContacts = () => Contact.find();
 
 export const createContact = (contact) => Contact.create(contact);
