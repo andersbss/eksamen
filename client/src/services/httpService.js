49f32efe7faa26d93c24eaad3ab23435e9e351a6
Sigmund <sigmund.baklia@gmail.com>
2020-12-07 11:57:23 +0100

    Gets csrf-token if request is POST or PUT

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,7 +1,19 @@
 import http from '../utils/http';
 
+export const getCsrfToken = async () => {
+  try {
+    const { data } = await http.get('/csrf-token');
+    http.defaults.headers['X-CSRF-Token'] = data.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
 export const request = async (method, endpoint, payload = null) => {
   try {
+    if (method === 'POST' || method === 'PUT') {
+      await getCsrfToken();
+    }
     return await http({ method, url: endpoint, data: payload });
   } catch (error) {
     if (error.response) return error.response;
