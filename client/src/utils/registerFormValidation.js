d391a83a8b30c167c20fc689cd79a714ef197597
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-07 12:33:23 +0100

    Added validation to register form

--- client/src/utils/registerFormValidation.js
+++ client/src/utils/registerFormValidation.js
@@ -3,9 +3,20 @@ const validate = (inputs) => {
   const errors = {};
 
   if (!firstName) errors.firstName = 'Fyll inn fornavn';
+
   if (!lastName) errors.lastName = 'Fyll inn etternavn';
+
   if (!email) errors.email = 'Fyll inn epost';
+  else if (!email.match(/\S+@\S+\.\S+/)) {
+    errors.email = 'Ugyldig e-postadresse';
+  }
+
   if (!password) errors.password = 'Fyll inn passord';
+  else if (password.length < 3)
+    errors.password = 'Passord må være minst tre tegn';
+  else if (!password.match(/(?=.*[0-9])/)) {
+    errors.password = 'Passord må inneholde minst ett nummer';
+  }
 
   return errors;
 };
