c43b89324fadf621c9d9a08479e9f988f19a6741
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-07 19:54:13 +0100

    Create service

--- server/src/services/userActivity.js
+++ server/src/services/userActivity.js
@@ -1,10 +1,13 @@
 import UserActivity from '../models/userActivity.js';
 
+export const createAuthor = (data) => UserActivity.create(data);
+
 export const getCountByArticle = async () => {
   const articles = await UserActivity.aggregate([
     {
       $group: { _id: '$article', count: { $sum: 1 } },
     },
   ]);
+  console.log(articles);
   return articles;
 };
