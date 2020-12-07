7d9d5575b87bf7c29aafcc198a23463329f64161
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-07 18:35:26 +0100

    Createt userActivity schema

---
+++ server/src/schemas/userActivity.js
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+import { OBJECT_ID_REGEX } from '../constants/regexes';
+
+export const userActivitySchema = Joi.object()
+  .keys({
+    user: Joi.string()
+      .regex(OBJECT_ID_REGEX)
+      .meta({ _mongoose: { type: 'ObjectId', ref: 'User' } })
+      .messages({
+        'string.pattern.base': 'User id is not valid',
+      }),
+    article: Joi.string()
+      .regex(OBJECT_ID_REGEX)
+      .meta({ _mongoose: { type: 'ObjectId', ref: 'Article' } }),
+  })
+  .options({ abortEarly: false });
