9632e36a5d307a751d36f19dcf7208fd1706b5cd
Sigmund <sigmund.baklia@gmail.com>
2020-12-07 17:56:09 +0100

    Test for button to be disabled if loading equals true

--- client/test/components/forms/ContactForm.test.jsx
+++ client/test/components/forms/ContactForm.test.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
 import ContactForm, {
   StyledSuccessMessage,
 } from '../../../src/components/forms/ContactForm';
 import Button from '../../../src/components/buttons/Button';
-import Textarea from '../../../src/components/common/Textarea';
 import Input from '../../../src/components/common/Input';
+import { theme } from '../../../src/styles/Theme';
 
 describe('<ContactForm />', () => {
   it('should render StyledSuccessMessage if submitSuccess is true', () => {
-    const isSuccess = true;
-    const wrapper = shallow(<ContactForm submitSuccess={isSuccess} />);
-
+    const wrapper = shallow(<ContactForm submitSuccess />);
     expect(wrapper.find(StyledSuccessMessage).exists()).toBe(true);
 
     wrapper.setProps({ submitSuccess: false });
-
     expect(wrapper.find(StyledSuccessMessage).exists()).toBe(false);
+  });
+
+  it('should disable button if loading is true', () => {
+    const wrapper = shallow(<ContactForm loading />);
+    expect(wrapper.find(Button).prop('disabled')).toEqual(true);
+
+    wrapper.setProps({ loading: false });
+    expect(wrapper.find(Button).prop('disabled')).toEqual(false);
   });
 });
