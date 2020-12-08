ce2a74a94eb25ed87cffa174dd36774ef0a262cb
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-08 09:41:27 +0100

    Created getTopArticle service and aggregation

--- server/src/services/userLog.js
+++ server/src/services/userLog.js
@@ -1,5 +1,52 @@
 import UserLog from '../models/userLog.js';
 
+const ARTICLE_LOOKUP = [
+  {
+    $lookup: {
+      from: 'articles',
+      localField: '_id',
+      foreignField: '_id',
+      as: 'article',
+    },
+  },
+  { $unwind: '$article' },
+  {
+    $lookup: {
+      from: 'categories',
+      localField: 'article.category',
+      foreignField: '_id',
+      as: 'category',
+    },
+  },
+  {
+    $unwind: '$category',
+  },
+  { $unwind: '$article' },
+  {
+    $lookup: {
+      from: 'authors',
+      localField: 'article.author',
+      foreignField: '_id',
+      as: 'author',
+    },
+  },
+  {
+    $unwind: '$author',
+  },
+  {
+    $project: {
+      article: '$_id',
+      count: '$count',
+      title: '$article.title',
+      ingress: '$article.ingress',
+      authorFirstName: '$author.firstName',
+      authorLastName: '$author.lastName',
+      category: '$category.title',
+      createdAt: '$article.createdAt',
+    },
+  },
+];
+
 export const createUserLog = (data) => UserLog.create(data);
 
 export const getCountByArticle = async () => {
@@ -7,38 +54,21 @@ export const getCountByArticle = async () => {
     {
       $group: { _id: '$article', count: { $sum: 1 } },
     },
+    ...ARTICLE_LOOKUP,
+  ]);
+  return articles;
+};
+
+export const getTopArticles = async () => {
+  const articles = await UserLog.aggregate([
     {
-      $lookup: {
-        from: 'articles',
-        localField: '_id',
-        foreignField: '_id',
-        as: 'article',
-      },
-    },
-    { $unwind: '$article' },
-    {
-      $lookup: {
-        from: 'categories',
-        localField: 'article.category',
-        foreignField: '_id',
-        as: 'category',
-      },
+      $group: { _id: '$article', count: { $sum: 1 } },
     },
     {
-      $unwind: '$category',
+      $sort: { count: -1 },
     },
-    { $unwind: '$article' },
-    {
-      $lookup: {
-        from: 'authors',
-        localField: 'article.author',
-        foreignField: '_id',
-        as: 'author',
-      },
-    },
-    {
-      $unwind: '$author',
-    },
+    { $limit: 10 },
+    ...ARTICLE_LOOKUP,
   ]);
   return articles;
 };
@@ -67,22 +97,4 @@ export const getCountByUser = async () => {
     },
   ]);
   return users;
-};
-
-export const getTopArticles = async () => {
-  const articles = await UserLog.aggregate([
-    {
-      $group: { _id: '$article', count: { $sum: 1 } },
-    },
-    {
-      $lookup: {
-        from: 'articles',
-        localField: '_id',
-        foreignField: '_id',
-        as: 'article',
-      },
-    },
-    { $unwind: '$article' },
-  ]);
-  return articles;
 };
