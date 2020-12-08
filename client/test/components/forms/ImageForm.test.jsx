c528d52756da6437862f44771b895e6d690c9545
Sigmund <sigmund.baklia@gmail.com>
2020-12-08 11:16:41 +0100

    ImageForm Test: it should call handleSubmit when form is submitted

---
+++ client/test/components/forms/ImageForm.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import ImageForm from '../../../src/components/forms/ImageForm';
+import Image from '../../../src/components/images/Image';
+
+describe('<ImageForm />', () => {
+  it('should call handleSubmit when form is submitted', () => {
+    const mockSubmit = jest.fn();
+    const wrapper = shallow(<ImageForm handleSubmit={mockSubmit} />);
+    wrapper.find('form').simulate('submit');
+    expect(mockSubmit).toHaveBeenCalled();
+  });
+});
