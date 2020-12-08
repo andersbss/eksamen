5531d615da006121e1164868441c5d3d5127d47c
Sigmund <sigmund.baklia@gmail.com>
2020-12-08 12:47:46 +0100

    PaginationButton Test: it should run setPage prop onClick

--- client/test/components/buttons/PaginationButton.test.jsx
+++ client/test/components/buttons/PaginationButton.test.jsx
@@ -9,4 +9,14 @@ describe('<PaginationButton />', () => {
     const wrapper = shallow(<PaginationButton number="1" />);
     expect(wrapper.find(StyledButton).text()).toContain('1');
   });
+
+  it('should run setPage prop onClick', () => {
+    const mockSetPage = jest.fn();
+
+    const wrapper = shallow(<PaginationButton setPage={mockSetPage} />);
+
+    expect(mockSetPage).toHaveBeenCalledTimes(0);
+    wrapper.find(StyledButton).simulate('click');
+    expect(mockSetPage).toHaveBeenCalledTimes(1);
+  });
 });
