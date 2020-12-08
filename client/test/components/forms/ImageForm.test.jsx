1f3b122f251735ca8889ff4366f37287a7222806
Sigmund <sigmund.baklia@gmail.com>
2020-12-08 11:18:55 +0100

    ImageForm Test: it should call handleChange when input field is changed

--- client/test/components/forms/ImageForm.test.jsx
+++ client/test/components/forms/ImageForm.test.jsx
@@ -10,4 +10,11 @@ describe('<ImageForm />', () => {
     wrapper.find('form').simulate('submit');
     expect(mockSubmit).toHaveBeenCalled();
   });
+
+  it('should call handleChange when input field is changed', () => {
+    const mockOnChange = jest.fn();
+    const wrapper = shallow(<ImageForm onChange={mockOnChange} />);
+    wrapper.find('input').simulate('change');
+    expect(mockOnChange).toHaveBeenCalled();
+  });
 });
