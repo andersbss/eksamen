f47f7f7e72fd428d5b98c333aa91ebf0e49ea943
Sigmund <sigmund.baklia@gmail.com>
2020-12-08 13:21:08 +0100

    Fixed descripe text

--- client/test/components/common/Jumbotron.test.jsx
+++ client/test/components/common/Jumbotron.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Jumbotron from '../../../src/components/common/Jumbotron';
 
-describe('<Input />', () => {
+describe('<Jumbotron />', () => {
   it('should contain correct headerText', () => {
     const wrapper = shallow(<Jumbotron headerText="this is a header" />);
     expect(wrapper.find('h1').text()).toContain('this is a header');
