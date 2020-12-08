4089440a22a166c442d47011c199bf8081ad1965
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-08 11:20:37 +0100

    Created static function for avg read time

--- server/src/models/article.js
+++ server/src/models/article.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from 'mongoose';
 import { articleSchema } from '../schemas/article';
 
+mongoose.set('useFindAndModify', false);
+
 const Joigoose = require('joigoose')(mongoose);
 
 const options = { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } };
@@ -16,6 +18,22 @@ const ArticleSchema = new Schema(
 );
 
 ArticleSchema.index({ title: 'text' });
+
+ArticleSchema.statics.calcReadTime = async function () {
+  const time = await this.aggregate([
+    { $sort: { createdAt: -1 } },
+    { $project: { words: { $split: ['$content', ' '] }, qty: 1 } },
+  ]);
+
+  await this.findOneAndUpdate(
+    { _id: time[0]._id },
+    { $set: { readTime: Math.round((time[0].words.length / 200 + Number.EPSILON) * 100) / 100 } }
+  );
+};
+
+ArticleSchema.post('save', async function () {
+  await this.constructor.calcReadTime();
+});
 
 ArticleSchema.virtual('userlogs', {
   ref: 'Article',
