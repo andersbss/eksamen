b99f88c8d09455bcaae40e505739546b935926d9
Sigmund <sigmund.baklia@gmail.com>
2020-12-08 13:14:37 +0100

    Tests all props passed to Input component

--- client/test/components/common/Input.test.jsx
+++ client/test/components/common/Input.test.jsx
@@ -17,4 +17,45 @@ describe('<Input />', () => {
     wrapper.setProps({ errorLabel: false });
     expect(wrapper.find('label').at(1).exists()).toBe(false);
   });
+
+  it('should have the correct type based on type prop', () => {
+    const wrapper = shallow(<Input type="button" />);
+    expect(wrapper.find('input').prop('type')).toEqual('button');
+  });
+
+  it('should have the correct name based on name prop', () => {
+    const wrapper = shallow(<Input name="input name" />);
+    expect(wrapper.find('input').prop('name')).toEqual('input name');
+  });
+
+  it('should have the correct maxLength based on maxLength prop', () => {
+    const wrapper = shallow(<Input maxLength="1337" />);
+    expect(wrapper.find('input').prop('maxLength')).toEqual('1337');
+  });
+
+  it('should have the correct placeholder based on placeholder prop', () => {
+    const wrapper = shallow(<Input placeholder="This is a placeholder" />);
+    expect(wrapper.find('input').prop('placeholder')).toEqual(
+      'This is a placeholder'
+    );
+  });
+
+  it('should have be required if required prop is true', () => {
+    const wrapper = shallow(<Input required />);
+    expect(wrapper.find('input').prop('required')).toEqual(true);
+    wrapper.setProps({ required: false });
+    expect(wrapper.find('input').prop('required')).toEqual(false);
+  });
+
+  it('should have the correct value based on the value prop', () => {
+    const wrapper = shallow(<Input value={123} />);
+    expect(wrapper.find('input').prop('value')).toEqual(123);
+  });
+
+  it('should call onChange passed by props, when changed', () => {
+    const mockOnChange = jest.fn();
+    const wrapper = shallow(<Input onChange={mockOnChange} />);
+    wrapper.find('input').simulate('change');
+    expect(mockOnChange).toHaveBeenCalled();
+  });
 });
