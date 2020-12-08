0060de155209c0c14422b7639417ecc8c2fe7287
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-08 14:56:31 +0100

    Fetching statistic data

--- client/src/pages/statistics/index.jsx
+++ client/src/pages/statistics/index.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from '../../hooks/useFetch';
 
-const Statistics = () => <h1>Stats</h1>;
+const Statistics = () => {
+  const {
+    error: userVisitsError,
+    loading: userVisitsLoading,
+    response: userVisitsResponse,
+    isSuccess: isUserVisitsSuccess,
+  } = useFetch('GET', 'userlog/uservisits');
+
+  const {
+    error: articleVisitsError,
+    loading: articleVisitsLoading,
+    response: articleVisitsResponse,
+    isSuccess: isArticleVisitsSuccess,
+  } = useFetch('GET', 'userlog/articlevisits');
+
+  const {
+    error: topArticlesError,
+    loading: topArticlesLoading,
+    response: topArticlesResponse,
+    isSuccess: isTopArticlesSuccess,
+  } = useFetch('GET', 'userlog/toparticles');
+
+  useEffect(() => {
+    console.log(userVisitsResponse);
+    console.log(articleVisitsResponse);
+  }, [userVisitsResponse, articleVisitsResponse]);
+
+  return <h1>Stats</h1>;
+};
 
 export default Statistics;
