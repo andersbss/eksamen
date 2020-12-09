3d810a3fe8f3f6341dc81c3bda505f1af315572a
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 10:50:02 +0100

    Setup article tests

--- server/test/integration/auth.test.js
+++ server/test/integration/auth.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import validator from 'validator';
 import { app } from '../../src/app.js';
-import { EMAIL_REGEX, OBJECT_ID_REGEX } from '../../src/constants/regexes.js';
+import { EMAIL_REGEX } from '../../src/constants/regexes.js';
 import { connectDatabase, closeDatabase, clearDatabase } from '../config/db.js';
 
 const BASE_URL = process.env.BASEURL;
