2d230f1e78064eb366bf822480327d1766939c3d
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 19:20:53 +0100

    Added data to contact item

--- client/src/components/items/ContactItem.jsx
+++ client/src/components/items/ContactItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledLi = styled.li``;
+const StyledLi = styled.li`
+  margin: 0;
+  padding: 10px;
+`;
 
 const ContactItem = ({ contact }) => {
   <StyledLi>
-    <h3>{}</h3>
+    <h3>{contact.name}</h3>
+    <p>{contact.email}</p>
+    <p>{contact.message}</p>
   </StyledLi>;
 };
+
+export default ContactItem;
