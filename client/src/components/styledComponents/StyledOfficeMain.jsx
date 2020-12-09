d276fe2bb2f1b5d006468580d227789152285498
Sigmund <sigmund.baklia@gmail.com>
2020-12-09 12:31:15 +0100

    Created StyledOfficeMain as main element for office grid and list view

---
+++ client/src/components/styledComponents/StyledOfficeMain.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledOfficeMain = styled.main`
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 50px;
+
+  & > h2 {
+    font-size: 4rem;
+    font-weight: 1000;
+    line-height: 0px;
+  }
+`;
+
+export default StyledOfficeMain;
