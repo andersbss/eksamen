de2f6e3acb020df9cc247a4d966ecd36dfedd75a
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 19:11:43 +0100

    Fetching contacts data

--- client/src/pages/contacts/index.jsx
+++ client/src/pages/contacts/index.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Jumbotron from '../../components/common/Jumbotron';
 import ContactsLayout from '../../layouts/ContactsLayout';
+import useFetch from '../../hooks/useFetch';
 
-const Contacts = () => (
-  <>
-    <Jumbotron headerText="Contacts" />
-    <ContactsLayout>
-      <h2>CONTACTS</h2>
-    </ContactsLayout>
-  </>
-);
+const Contacts = () => {
+  const { error, loading, response } = useFetch('GET', 'contacts');
+
+  useEffect(() => {
+    console.log(response);
+  }, [response]);
+
+  return (
+    <>
+      <Jumbotron headerText="Contacts" />
+      <ContactsLayout>
+        <h2>CONTACTS</h2>
+      </ContactsLayout>
+    </>
+  );
+};
 
 export default Contacts;
