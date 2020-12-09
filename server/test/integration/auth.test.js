586daa67bd5497886e4301b12061ba24129f145d
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 08:33:27 +0100

    More setup in auth test

--- server/test/integration/auth.test.js
+++ server/test/integration/auth.test.js
@@ -6,6 +6,7 @@ import { connectDatabase, closeDatabase, clearDatabase } from '../config/db.js';
 const BASE_URL = process.env.BASEURL;
 
 let userPayload;
+let userRes;
 
 const resetUserPayLoad = (emailParam = 'test@test.com') => {
   userPayload = {
@@ -20,10 +21,19 @@ beforeAll(async () => {
   await connectDatabase();
 });
 
+beforeEach(async () => {
+  resetUserPayLoad();
+  userRes = await request(app).post(`${BASE_URL}/register`).send(userPayload);
+});
+
 afterAll(async (done) => {
   await closeDatabase(done);
 });
 
 afterEach(async () => {
   await clearDatabase();
+});
+
+describe('Auth', () => {
+  it('should return a new user including valid jwt', async () => {});
 });
