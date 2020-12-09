6a4f2bd0dd16eda378fd4395ac3c0a3900c29181
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 14:13:45 +0100

    should return success wit all private articles

--- server/test/integration/article.test.js
+++ server/test/integration/article.test.js
@@ -151,4 +151,19 @@ describe('Get', () => {
     expect(data.currentPage).toEqual(1);
     expect(data.data[0].public).toBe(true);
   });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('should return success wit all private articles', async () => {
+    await request(app).post(`${BASE_URL}/articles`).set('Cookie', `token=${token}`).send(articlePayload);
+    const privateArticlesRes = await request(app).get(`${BASE_URL}/articles`).set('Cookie', `token=${token}`);
+
+    const { success, data } = privateArticlesRes.body;
+
+    expect(privateArticlesRes.status).toEqual(200);
+    expect(success).toBe(true);
+    expect(data.data).toHaveLength(1);
+    expect(data.totalPages).toEqual(1);
+    expect(data.currentPage).toEqual(1);
+    expect(data.data[0].public).toBe(false);
+  });
 });
