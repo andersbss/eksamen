5cb98580860ab5d230976f3cc2cd7b8a2773dc5f
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-09 15:48:19 +0100

    should return user without id and password when given token

--- server/src/services/user.js
+++ server/src/services/user.js
@@ -2,7 +2,10 @@ import User from '../models/user.js';
 
 export const createUser = async (user) => User.create(user);
 
-export const getUserById = async (id) => User.findById(id);
+export const getUserById = async (id, excludeId) => {
+  if (excludeId) return User.findById(id, { _id: 0 });
+  return User.findById(id);
+};
 
 export const getUserByEmail = async (email, includePassword) => {
   if (includePassword) return User.findOne({ email }).select('+password');
--- server/test/integration/user.test.js
+++ server/test/integration/user.test.js
@@ -1,3 +1,4 @@
+import expectCt from 'helmet/dist/middlewares/expect-ct';
 import request from 'supertest';
 import { app } from '../../src/app.js';
 import { OBJECT_ID_REGEX } from '../../src/constants/regexes.js';
@@ -5,11 +6,26 @@ import { connectDatabase, closeDatabase, clearDatabase } from '../config/db.js';
 
 const BASE_URL = process.env.BASEURL;
 
+let adminRes;
+let token;
+
+const userPayload = {
+  firstName: 'test',
+  lastName: 'test',
+  email: 'admin@admin.com',
+  password: 'SecretPassword879',
+  role: 'admin',
+};
+
 beforeAll(async () => {
   await connectDatabase();
 });
 
-beforeEach(async () => {});
+beforeEach(async () => {
+  adminRes = await request(app).post(`${BASE_URL}/register`).send(userPayload);
+
+  token = adminRes.body.data.token;
+});
 
 afterAll(async () => {
   await closeDatabase();
@@ -17,4 +33,24 @@ afterAll(async () => {
 
 afterEach(async () => {
   await clearDatabase();
+});
+
+describe('Get', () => {
+  // eslint-disable-next-line jest/expect-expect
+  it('should return user without id when given token', async () => {
+    const response = await request(app).get(`${BASE_URL}/me`).set('Cookie', `token=${token}`);
+
+    const { success, data } = response.body;
+
+    expect(1).toEqual(1);
+    expect(success).toBe(true);
+    expect(response.status).toEqual(200);
+    expect(data.role).toEqual(userPayload.role);
+    expect(data.firstName).toEqual(userPayload.firstName);
+    expect(data.lastName).toEqual(userPayload.lastName);
+    expect(data.email).toEqual(userPayload.email);
+    expect(data._id).toEqual(undefined);
+    expect(data.password).toEqual(undefined);
+    expect(data.id).toEqual(null);
+  });
 });
