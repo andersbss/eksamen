2e8addc5d22f300dfcd3ffa193ddb08e3610858a
Sigmund <sigmund.baklia@gmail.com>
2020-12-10 11:36:08 +0100

    OfficeSelectFilter Test: it should contain correct headerOption

--- client/test/components/common/OfficeSelectFilter.test.jsx
+++ client/test/components/common/OfficeSelectFilter.test.jsx
@@ -34,4 +34,26 @@ describe('<OfficeSelectFilter />', () => {
     wrapper.find(StyledSelect).simulate('change');
     expect(mockOnChange).toHaveBeenCalled();
   });
+
+  it('should only have "FILTER" option when no filter is chosen', () => {
+    const locations = ['Norway', 'Sweden', 'Denmark'];
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <OfficeSelectFilter locations={locations} chosenLocation="INGEN" />
+      </ThemeProvider>
+    );
+
+    expect(wrapper.find('option').at(0).text()).toContain('FILTER');
+  });
+
+  it('should only have "INGEN" option when no filter is chosen', () => {
+    const locations = ['Norway', 'Sweden', 'Denmark'];
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <OfficeSelectFilter locations={locations} chosenLocation="FILTER" />
+      </ThemeProvider>
+    );
+
+    expect(wrapper.find('option').at(0).text()).toContain('INGEN');
+  });
 });
