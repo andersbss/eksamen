8f47d7fa77c4e1f5748976a37109972a77f1b1c8
Sigmund <sigmund.baklia@gmail.com>
2020-12-10 15:15:51 +0100

    Passes required functions to ContactForm based on prop validation / removed no longer needed button render test

--- client/test/components/forms/ContactForm.test.jsx
+++ client/test/components/forms/ContactForm.test.jsx
@@ -2,40 +2,61 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ContactForm from '../../../src/components/forms/ContactForm';
 import { StyledSuccessMessage } from '../../../src/components/styledComponents/StyledMessages';
-import Button from '../../../src/components/buttons/Button';
+import Button from '../../../src/components/styledComponents/StyledButton';
 import Input from '../../../src/components/common/Input';
+
+const mockHandleSubmit = jest.fn();
+const mockHandleChange = jest.fn();
+const mockSetInput = jest.fn();
 
 describe('<ContactForm />', () => {
   it('should only render StyledSuccessMessage if submitSuccess is true', () => {
-    const wrapper = shallow(<ContactForm submitSuccess />);
+    const wrapper = shallow(
+      <ContactForm
+        submitSuccess
+        handleSubmit={mockHandleSubmit}
+        handleChange={mockHandleChange}
+      />
+    );
     expect(wrapper.find(StyledSuccessMessage).exists()).toBe(true);
 
     wrapper.setProps({ submitSuccess: false });
     expect(wrapper.find(StyledSuccessMessage).exists()).toBe(false);
   });
 
-  it('should only render Button if submitSuccess is false', () => {
-    const wrapper = shallow(<ContactForm submitSuccess={false} />);
-    expect(wrapper.find(Button).exists()).toBe(true);
-
-    wrapper.setProps({ submitSuccess: true });
-    expect(wrapper.find(Button).exists()).toBe(false);
-  });
-
   it('should show correct error message if error is true', () => {
     const errorMessage = 'Something unexpected happened!';
-    const wrapper = shallow(<ContactForm error={errorMessage} />);
+    const wrapper = shallow(
+      <ContactForm
+        error={errorMessage}
+        handleSubmit={mockHandleSubmit}
+        handleChange={mockHandleChange}
+      />
+    );
     expect(wrapper.find('p').text()).toContain(errorMessage);
   });
 
-  it('should disable button if loading or hasErrors is true', () => {
-    const wrapper = shallow(<ContactForm loading={false} hasErrors={false} />);
+  it('should disable button if loading, hasErrors or submitSuccess is true', () => {
+    const wrapper = shallow(
+      <ContactForm
+        loading={false}
+        hasErrors={false}
+        submitSuccess={false}
+        handleSubmit={mockHandleSubmit}
+        handleChange={mockHandleChange}
+        setInputs={mockSetInput}
+      />
+    );
+
     expect(wrapper.find(Button).prop('disabled')).toEqual(false);
 
     wrapper.setProps({ loading: true });
     expect(wrapper.find(Button).prop('disabled')).toEqual(true);
 
     wrapper.setProps({ loading: false, hasErrors: true });
+    expect(wrapper.find(Button).prop('disabled')).toEqual(true);
+
+    wrapper.setProps({ loading: false, hasErrors: false, submitSuccess: true });
     expect(wrapper.find(Button).prop('disabled')).toEqual(true);
   });
 
@@ -44,7 +65,12 @@ describe('<ContactForm />', () => {
     const userName = 'user name';
 
     const wrapper = shallow(
-      <ContactForm userEmail={email} userName={userName} />
+      <ContactForm
+        userEmail={email}
+        userName={userName}
+        handleSubmit={mockHandleSubmit}
+        handleChange={mockHandleChange}
+      />
     );
 
     expect(wrapper.find(Input).at(0).prop('defaultValue')).toEqual(email);
@@ -54,7 +80,9 @@ describe('<ContactForm />', () => {
   it('should call handleSubmit when form is submitted', () => {
     const mockSubmit = jest.fn();
 
-    const wrapper = shallow(<ContactForm handleSubmit={mockSubmit} />);
+    const wrapper = shallow(
+      <ContactForm handleSubmit={mockSubmit} handleChange={mockHandleChange} />
+    );
     wrapper.simulate('submit');
     expect(mockSubmit).toHaveBeenCalled();
   });
