9012134cf7851689e5107b6d6d840414a8cfc883
andersbss <43789401+andersbss@users.noreply.github.com>
2020-12-10 21:59:57 +0100

    Fixed api test bug

--- server/src/routes/author.js
+++ server/src/routes/author.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', authorController.getAll);
 
-// This was only used to put data into the database
-// router.post('/', authorController.create);
+// This was only used to put data into the database and for testing.
+router.post('/', authorController.create);
 
 export default router;
