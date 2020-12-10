60074bcbc15b2e2e82eb86c13d22965d7a04abef
Sigmund <sigmund.baklia@gmail.com>
2020-12-10 13:10:53 +0100

    Added authentication and authorization to image upload

--- server/src/routes/image.js
+++ server/src/routes/image.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { imageController } from '../controllers/index.js';
 import { upload } from '../middleware/image.js';
+import { authenticate, authorize } from '../middleware/auth.js';
+import { ROLE } from '../constants/roles.js';
 
 const router = express.Router();
 
-// Skal bare admin kunne uploade? Isåfall legge til authentication
-router.post('/upload', upload, imageController.create);
+router.post('/upload', authenticate, authorize(ROLE.ADMIN, ROLE.SUPER_ADMIN), upload, imageController.create);
 router.get('/download/:id', imageController.get);
 
 export default router;
