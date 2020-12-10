5006129e47b496e3fd5fc368f9ff2a463b84ccbb
Sigmund <sigmund.baklia@gmail.com>
2020-12-10 10:45:15 +0100

    improved CategorySelectFilter test

--- client/test/components/common/CategorySelectFilter.test.jsx
+++ client/test/components/common/CategorySelectFilter.test.jsx
@@ -10,15 +10,15 @@ describe('<CategorySelectFilter />', () => {
     // 3 categories, + 1 header(default) <option> = 4
     const categories = [
       {
-        _id: '1',
+        _id: 1,
         title: 'category1',
       },
       {
-        _id: '2',
+        _id: 2,
         title: 'category2',
       },
       {
-        _id: '3',
+        _id: 3,
         title: 'category3',
       },
     ];
@@ -26,11 +26,19 @@ describe('<CategorySelectFilter />', () => {
 
     expect(wrapper.find('option').length).toEqual(4);
     expect(wrapper.text()).toContain('category1' && 'category2' && 'category3');
+    expect(wrapper.find('option').at(1).prop('value')).toEqual(1);
+    expect(wrapper.find('option').at(2).prop('value')).toEqual(2);
+    expect(wrapper.find('option').at(3).prop('value')).toEqual(3);
   });
 
   it('should call setChosenCategory when selected is changed', () => {
     const mockOnChange = jest.fn();
-    const categories = ['category1', 'category2', 'category3'];
+    const categories = [
+      {
+        _id: '1',
+        title: 'category1',
+      },
+    ];
 
     const wrapper = mount(
       <ThemeProvider theme={theme}>
